7a6d25430d7e3c73e39e52f68549a1723153615d
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2022-01-16 11:57:35 +0900

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -14,24 +14,18 @@ export default [
   {
     input: 'src/index.ts',
     output: {
-      name: 'html-component',
       file: 'dist/component.js',
       format: 'iife',
       sourcemap: true,
-      exports: 'named',
-      extend: true,
     },
     plugins,
   },
   {
     input: 'src/init.ts',
     output: {
-      name: 'html-component-init',
       file: 'dist/component-init.js',
       format: 'iife',
       sourcemap: true,
-      exports: 'named',
-      extend: true,
     },
     plugins,
   },
